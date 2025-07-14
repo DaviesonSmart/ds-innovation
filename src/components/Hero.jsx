b267ccd7e12af94c332fd646ad280e7266c25eb4
Davieson Smart <engrdavieson@gmail.com>
2025-07-14 23:25:57 +0100

    🛠️ Fix import case to match image file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
-import dress9Img from "../assets/dress9.JPG"; // Make sure to add a real image here
+import dress9Img from "../assets/dress9.jpg"; // Make sure to add a real image here
 import { toast } from "react-toastify";
 
 export default function Hero() {
